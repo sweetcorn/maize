1f3090118cea073f4b147d643b61445892842cc6
PJ Kelly <me@pjkel.ly>
2013-08-28 16:48:39 -0700

    More deployment tweaks.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,6 @@
 set :application, "ibm-knowledge-editions-app"
-
+set :deploy_to, "/srv/#{application}"   #capper defaults to "/var/app/#{application}"
 set :repository, "git@github.com:crushlovely/ibm-knowledge-editions-app.git"
-ssh_options[:forward_agent] = true
-default_run_options[:pty] = true
-
-on :start do
-  `ssh-add`
-end
 
 set :scm, :git                                      #capper default
 set :user, 'deploy'
@@ -14,7 +8,9 @@ set :user, 'deploy'
 set :use_sudo, false                                #capper default
 set :keep_releases, 5                               #capper default
 set :deploy_via, :remote_cache                      #capper default
+
 set :main_js, "app.js"
+set :forever_cmd, "./node_modules/.bin/forever"           #use the forever that is installed along with the app
 
 # your log folder to share between different releases
 # you can add more folders here...
@@ -22,11 +18,17 @@ set :symlinks, {"log" => "log"}
 
 stage :production do
   set :node_env, 'production'
-  server "em-sj1.valuedrivenprovocation.com", :app, :primary => true
+  server "192.168.33.10", :app, :primary => true
 end
 
 desc "tail the application logfile"
 task :log do
   log = "#{application_dir}/current/log/#{node_env}.log"
   run "tail -f #{log}"
+end
+
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+on :start do
+  `ssh-add`
 end

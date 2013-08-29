c20b1f7890381508bf1dcce2996b790a6f83d8da
PJ Kelly <me@pjkel.ly>
2013-08-29 13:36:53 -0700

    Important conversations to have JSON.

--- assets/js/json/editions/cac2b27c-fa00-400d-a644-ba6408b2566d/containers/index.js
+++ assets/js/json/editions/cac2b27c-fa00-400d-a644-ba6408b2566d/containers/index.js
@@ -2,6 +2,7 @@ module.exports = [
   require('./0af17960-db7a-4551-a6f0-e6ed5145d9e8.json'),
   require('./95017953-cca4-4d11-acb8-13ec5d657cde.json'),
   require('./ccab7c6e-ec15-41d1-abcf-2f4a93c1ede9.json'),
+  require('./e81ca67c-e932-428f-b83e-fc1d73cd1f13.json'),
   require('./e8b0be7b-eebc-4a4c-9824-a78238947eda.json'),
   require('./e9ddaad3-83c6-4662-a461-4be70d765846.json'),
   require('./fb4a9e17-6665-4d48-a964-a82c50cde47a.json')

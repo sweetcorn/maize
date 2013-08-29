e629766ebcf0d31a796db29006a5cc7409032e56
PJ Kelly <me@pjkel.ly>
2013-08-28 17:54:15 -0700

    Using standard capper. Customized npm installation.

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,3 +32,27 @@ default_run_options[:pty] = true
 on :start do
   `ssh-add`
 end
+
+after 'deploy:update_code', "npm:install"
+
+namespace :npm do
+  desc <<-DESC
+    Install the current npm environment. \
+    Note it is recommended to check in npm-shrinkwrap.json into version control\
+    and to put node_module into .gitignore to manage dependencies. \
+    See http://blog.nodejs.org/2012/02/27/managing-node-js-dependencies-with-shrinkwrap \
+    If no npm-shrinkwrap.json is found packages are installed from package.json with no guarantee\
+    about the version beeing installed
+    If the npm cmd cannot be found then you can override the npm_cmd variable to specifiy \
+    which one it should use.
+
+    You can override any of these defaults by setting the variables shown below.
+
+      set :npm_cmd,      "npm" # e.g. "/usr/local/bin/npm"
+      set :npm_flags,    "--production --loglevel warn" # e.g. "--production --loglevel info"
+  DESC
+  task :install, :roles => :app, :except => { :no_release => true } do
+    run("cd #{latest_release} && #{fetch(:npm_cmd, "npm")} install #{fetch(:npm_flags, "--production --loglevel warn")} ")
+  end
+end
+

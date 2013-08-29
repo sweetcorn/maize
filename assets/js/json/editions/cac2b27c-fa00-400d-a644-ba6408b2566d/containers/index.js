03c4f3e50845346c90acfbbea80c190c64bca07b
PJ Kelly <me@pjkel.ly>
2013-08-29 15:42:29 -0700

    Added JSON for Partnering options to explore.

--- assets/js/json/editions/cac2b27c-fa00-400d-a644-ba6408b2566d/containers/index.js
+++ assets/js/json/editions/cac2b27c-fa00-400d-a644-ba6408b2566d/containers/index.js
@@ -2,6 +2,7 @@ module.exports = [
   require('./0af17960-db7a-4551-a6f0-e6ed5145d9e8.json'),
   require('./8a18a90e-e55d-4c1c-955c-8ef66e27e4a5.json'),
   require('./95017953-cca4-4d11-acb8-13ec5d657cde.json'),
+  require('./ab2eecc6-a50e-41e9-ba8a-f6cc2e15db6e.json'),
   require('./ccab7c6e-ec15-41d1-abcf-2f4a93c1ede9.json'),
   require('./e81ca67c-e932-428f-b83e-fc1d73cd1f13.json'),
   require('./e8b0be7b-eebc-4a4c-9824-a78238947eda.json'),

203a516530098528a39e93608d3ec67168d2a55a
Justin Reidy <jmreidy@rzrsharp.net>
2013-08-30 13:49:23 -0400

    make sure app is launched on prod

--- app.js
+++ app.js
@@ -41,8 +41,10 @@ app.get('/', routes.index);
 app.get('/editions/:edition_id', routes.index);
 app.get('/editions/:edition_id/containers/:container_id', routes.index);
 
-// http.createServer(app).listen(app.get('port'), function(){
-//   console.log('Express server listening on port ' + app.get('port'));
-// });
+if (process.env.NODE_ENV === 'production') {
+  http.createServer(app).listen(app.get('port'), function(){
+     console.log('Express server listening on port ' + app.get('port'));
+  });
+}
 
 module.exports = app;
